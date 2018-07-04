9a363d9267fc28dac50d7dc2e8838c49ce24264d
bluemonkey1130 <bluemonkey1130@gmail.com>
2018-07-04 10:02:09 +0100

    added hero Carousel to index.php
    Signed-off-by: bluemonkey1130 <bluemonkey1130@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 (function ($) {
 
 
-
+    $('.slider').slick({
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows:false,
+        dots:false,
+        fade:true,
+    });
 
 
 })(jQuery); // Fully reference jQuery after this point.
